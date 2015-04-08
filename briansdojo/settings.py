bc6a2dbf74dcdcbaed2e00c040dac4b3a859ac43
Brian Wilson <bwilson@twig-world.com>
2015-04-08 20:40:34 +0100

    Set database name in settings

--- briansdojo/settings.py
+++ briansdojo/settings.py
@@ -60,7 +60,7 @@ WSGI_APPLICATION = 'briansdojo.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'briansdojo.sqlite3'),
     }
 }
 
