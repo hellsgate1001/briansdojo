96241e7e7f980f754dcadf84123b6949e3565f52
= <=>
2015-04-16 11:56:28 +0100

    Update the wsgi

--- briansdojo/wsgi.py
+++ briansdojo/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for briansdojo project.
+WSGI config for thatforum project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -7,8 +7,30 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
-import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "briansdojo.settings")
+import os, sys, site
+sys.path.append('/var/envs/briansdojo/lib/python2.7/site-packages')
+from unipath import Path
+sys.path.append('/var/projects/briansdojo')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'briansdojo.settings'
 
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+import django.core.handlers.wsgi
+
+_application = django.core.handlers.wsgi.WSGIHandler()
+
+def application(environ, start_response):
+    try:
+        return _application(environ, start_response)
+    except ImportError:
+        # Remember the original sys.path
+        prev_sys_path = sys.path
+        # Add the project folder to path
+        mp = Path(__file__).ancestor(2)
+        site.addsitedir(mp)
+        # Reorder sys.path so new directory is at front
+        new_sys_path = []
+        for item in list(sys.path):
+            if item not in prev_sys_path:
+                new_sys_path.append(item)
+                sys.path.remove(item)
+        sys.path[:0] = new_sys_path
+        return _application(environ, start_response)
